840587df1113c2f0549b17ad8703aca996f26d9c
Cool-candy <coolcandywholesquare@gmail.com>
2024-11-12 12:51:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-var http=require('http');
-var dt=require('./demomodule');
-var fs=require('fs')
+var http = require('http');
 
-http.createServer(function mmain(req,res){
-fs.readFile('attempt.html',function(err,data){
-res.writeHead(200,{'Content-Type':'text/html'});
-res.write("The date & time are currently:" + dt.myDateTime());
-res.write(data);
-return res.end('<h1 style="color:red;">!!!!&#2471;&#2497;&#2468;&#2509;&#2468;&#2507;&#2480;&#2495;!!!!</h1>');
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
 }).listen(8080);
